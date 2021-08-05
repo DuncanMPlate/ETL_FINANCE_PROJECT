9ba526b47c2a5cd7fe2a323f2c2f95156c45ceb2
Duncan Plate <duncanplate@gmail.com>
2021-08-05 00:43:38 -0500

    setting up databases

---
+++ queries.sql
@@ -0,0 +1,23 @@
+Create Table indexData (
+	id int Primary Key,
+	"Index" varchar(30),
+	Date varchar(30),
+	"Open" decimal,
+	"High" decimal, 
+	"Low" decimal,
+	"Close" decimal,
+	"Adj Close" decimal,
+	"Volume" decimal
+);
+
+Create Table indexInfo (
+	id int Primary key,
+	"Region" varchar(50) not null,
+	"Exchange" varchar(100) not null,
+	"Index" varchar(30) not null,
+	"Currency" varchar(10) not null
+);
+
+SELECT * from indexData
+join indexInfo
+on indexData."Index" = indexInfo."Index"
